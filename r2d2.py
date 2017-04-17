a37d6209a9cc7949b261b7540ae74f8b89370468
nate <nate.moore@mooreproject.com>
2017-04-17 16:19:37 -0400

    Match VAF scenarios to appropriate classes. Output results.

--- r2d2.py
+++ r2d2.py
@@ -1,7 +1,10 @@
 import argparse
 import pandas as pd
-import ConfigParser
-from scenarios import Condition
+from scenarios import Scenario
+from collections import OrderedDict
+
+AF_COLUMN = 'AF1'
+OUTPUT_FILE = 'output.tsv'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,6 +14,7 @@ if __name__ == "__main__":
     parser.add_argument('-rt', '--rna_tumor', type=argparse.FileType('r'), help='Path to RNA tumor MAF.')
     args = parser.parse_args()
 
+    # Load all input files into pandas DFs.
     read_csv_args = {'sep': '\t', 'comment': '#', 'skip_blank_lines': True, 'header': 0}
     inputs = {
         'dna_normal': pd.read_csv(args.dna_normal, **read_csv_args),
@@ -19,6 +23,7 @@ if __name__ == "__main__":
         'rna_tumor': pd.read_csv(args.rna_tumor, **read_csv_args)
     }
 
+    # Merge DFs on position into one agg DF.
     inputs_merge = inputs['dna_normal'].merge(
         inputs['dna_tumor'],
         how='outer',
@@ -39,9 +44,52 @@ if __name__ == "__main__":
         suffixes=['_rn', '_rt']
     )
 
-    print inputs_merge
+    # Extract dna_normal/dna_tumor/rna_normal/rna_tumor values from DF.
+    output_maf_map = OrderedDict([
+        ('Hugo_Symbol', 'Hugo_Symbol_dn'),
+        ('Chromosome', 'Chromosome_dn'),
+        ('Start_position', 'Start_position'),
+        ('End_position', 'End_position'),
+        ('Strand', 'Strand_dn'),
+        ('Variant_Classification', 'Variant_Classification_dn'),
+        ('Variant_Type', 'Variant_Type_dn'),
+        ('Reference_Allele', 'Reference_Allele_dn'),
+        ('DNA_Normal_Allele1', 'Tumor_Seq_Allele1_dn'),
+        ('DNA_Normal_Allele2', 'Tumor_Seq_Allele2_dn'),
+        ('DNA_Tumor_Allele1', 'Tumor_Seq_Allele1_dt'),
+        ('DNA_Tumor_Allele2', 'Tumor_Seq_Allele2_dt'),
+        ('RNA_Normal_Allele1', 'Tumor_Seq_Allele1_rn'),
+        ('RNA_Normal_Allele2', 'Tumor_Seq_Allele2_rn'),
+        ('RNA_Tumor_Allele1', 'Tumor_Seq_Allele1_rt'),
+        ('RNA_Tumor_Allele2', 'Tumor_Seq_Allele2_rt'),
+    ])
 
-    # Load all input files into pandas DFs.
-    # Merge DFs on position into one agg DF.
-    # Have factory function that creates a scenario class (RNAed/LOH/etc.) based on a single row from above DF.
+    output_rows = OrderedDict()
+    for output_column in ['scenario'] + output_maf_map.keys():
+        output_rows[output_column] = []
+
+    for i, row in inputs_merge.iterrows():
+        dna_normal = row['AF1_dn'] if 'AF1_dn' in row.keys() else 0
+        dna_tumor = row['AF1_dt'] if 'AF1_dt' in row.keys() else 0
+        rna_normal = row['AF1_rn'] if 'AF1_rn' in row.keys() else 0
+        rna_tumor = row['AF1_rt'] if 'AF1_rt' in row.keys() else 0
+
+        quad = {
+            'dna_normal': dna_normal,
+            'dna_tumor': dna_tumor,
+            'rna_normal': rna_normal,
+            'rna_tumor': rna_tumor,
+        }
+
+        try:
+            scenario = Scenario(quad)
+        except Scenario.NoScenarioException as e:
+            continue
+
+        output_rows['scenario'].append(scenario.name)
+        for output_column in output_maf_map:
+            input_column = output_maf_map[output_column]
+            output_rows[output_column].append(row[input_column])
+
     # Write output.
+    pd.DataFrame.from_dict(output_rows).to_csv(OUTPUT_FILE, index=False, header=True, sep='\t')
--- scenarios.py
+++ scenarios.py
@@ -1,11 +1,15 @@
+import ConfigParser
+
+SCENARIOS_CONFIG = 'scenarios.ini'
+
 class Condition(object):
     conditions_types = ['=', '<', '<=', '>', '>=']
 
-    def __init__(self, conditions_str):
-        self.conditions = []
+    def __init__(self, condition_str):
+        self._clauses = []
 
-        for condition_str in conditions_str.split('|'):
-            tokens = condition_str.strip().split(' ')
+        for clause in condition_str.split('|'):
+            tokens = clause.strip().split(' ')
             tokens = [tok.strip() for tok in tokens]
 
             if len(tokens) == 2:
@@ -13,24 +17,127 @@ class Condition(object):
             elif len(tokens) == 3:
                 threshold = (float(tokens[1]), float(tokens[2]))
             else:
-                raise ValueError('Malformed conditions string - incorrect length (%s)' % conditions_str)
+                raise ValueError('Malformed conditions string - incorrect length (%s)' % condition_str)
 
             if tokens[0] not in Condition.conditions_types:
-                raise ValueError('Malformed conditions string - incorrect type (%s)' % conditions_str)
+                raise ValueError('Malformed conditions string - incorrect type (%s)' % condition_str)
 
-            self.conditions.append({'type': tokens[0], 'threshold': threshold})
+            self._clauses.append({'type': tokens[0], 'threshold': threshold})
 
     def test(self, value):
         value = float(value)
-        for condition in self.conditions:
+        for clause in self._clauses:
             if (
-                (condition['type'] == '<' and value < condition['threshold']) or
-                (condition['type'] == '<=' and value <= condition['threshold']) or
-                (condition['type'] == '>' and value > condition['threshold']) or
-                (condition['type'] == '>=' and value >= condition['threshold']) or
-                (condition['type'] == '=' and
-                    (value >= condition['threshold'][0] and value <= condition['threshold'][1]))
+                (clause['type'] == '<' and value < clause['threshold']) or
+                (clause['type'] == '<=' and value <= clause['threshold']) or
+                (clause['type'] == '>' and value > clause['threshold']) or
+                (clause['type'] == '>=' and value >= clause['threshold']) or
+                (clause['type'] == '=' and clause['threshold'][0] <= value <= clause['threshold'][1])
                ):
                 return True
 
         return False
+
+
+class Scenario(object):
+    class NoScenarioException(Exception):
+        pass
+
+    # Map of: {'scenario ini config name': [Conditions], ...}
+    condition_set = None
+
+    @classmethod
+    def _get_all_subclasses(cls):
+        for subclass in cls.__subclasses__():
+            yield subclass
+
+            for subclass in subclass._get_all_subclasses():
+                yield subclass
+
+    @classmethod
+    def _set_subclass_conditions(cls, config):
+        for subclass in Scenario._get_all_subclasses():
+            subclass.conditions = {}
+            if subclass.name in config.sections():
+                for item in config.items(subclass.name):
+                    subclass.conditions[item[0]] = Condition(item[1])
+
+    _subclass_conditions_set = False
+
+    def __new__(cls, quad):
+        if not Scenario._subclass_conditions_set:
+            config = ConfigParser.ConfigParser()
+            config.read(SCENARIOS_CONFIG)
+            Scenario._set_subclass_conditions(config)
+
+            Scenario._subclass_conditions_set = True
+
+        for subclass in Scenario._get_all_subclasses():
+            remaining_types = subclass.conditions.keys()
+            for quad_type, quad_vaf in quad.iteritems():
+                if quad_type in remaining_types:
+                    if not subclass.conditions[quad_type].test(quad_vaf):
+                        break
+
+                    remaining_types.remove(quad_type)
+
+                if not remaining_types:
+                    return object.__new__(subclass)
+
+        raise Scenario.NoScenarioException('No matching Scenario for quad %s' % quad)
+
+
+class RNAedAllInputs(Scenario):
+    name = 'rnaed_all_inputs'
+
+
+class RNAedNormalOnly(Scenario):
+    name = 'rnaed_normal_only'
+
+
+class TRNAedAllInputs(Scenario):
+    name = 't_rnaed_all_inputs'
+
+
+class TRNAedNoDNANormal(Scenario):
+    name = 't_rnaed_no_dna_normal'
+
+
+class VSEAllInputs(Scenario):
+    name = 'vse_all_inputs'
+
+
+class VSENormalOnly(Scenario):
+    name = 'vse_normal_only'
+
+
+class TVSEAllInputs(Scenario):
+    name = 't_vse_all_inputs'
+
+
+class TVSENoDNANormal(Scenario):
+    name = 't_vse_no_dna_normal'
+
+
+class TVSLAllInputs(Scenario):
+    name = 't_vsl_all_inputs'
+
+
+class TVSLNoDNANormal(Scenario):
+    name = 't_vsl_no_dna_normal'
+
+
+class LOHAllInputs(Scenario):
+    name = 'loh_all_inputs'
+
+
+class LOHDNAOnly(Scenario):
+    name = 'loh_dna_only'
+
+
+class SomaticAllInputs(Scenario):
+    name = 'somatic_all_inputs'
+
+
+class SomaticDNAOnly(Scenario):
+    name = 'somatic_dna_only'
